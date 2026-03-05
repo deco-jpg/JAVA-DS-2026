7163fbbe20565cea4c91a14b67ed90dfc3009ed9
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-04 21:09:57 -0300

    Encapsulamento

---
+++ poo/src/main/java/com/example/relacionamentos/UnidadeFederativa.java
@@ -0,0 +1,31 @@
+package com.example.relacionamentos;
+
+public enum UnidadeFederativa {
+    BAHIA("Bahia", "BA"),
+    SAO_PAULO("São Paulo", "SP"),
+    RIO_DE_JANEIRO("Rio de Janeiro", "RJ");
+
+    private String nome;
+    private String sigla;
+
+
+    private UnidadeFederativa(String nome, String sigla) {
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    
+
+    
+
+}
