ef01672f6c6ecd2aa19fa27f39bb94f429f667d8
deco-jpg <srdaniloh@gmail.com>
2026-03-10 19:30:58 -0300

    Add files via upload

---
+++ Heranca/src/main/java/org/example/exemplo1/Cliente.java
@@ -0,0 +1,39 @@
+package org.example.exemplo1;
+
+public class Cliente extends Pessoa {  //extends buscar na classe selecionada
+
+    private String dataCompra;
+    private String formaPagamento;
+
+    public Cliente(String nome, int idade, String dataCompra, String formaPagamento) {
+        super(nome, idade);
+        this.dataCompra = dataCompra;
+        this.formaPagamento = formaPagamento;
+    }
+
+    public String getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(String dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "dataCompra='" + dataCompra + '\'' +
+                ", formaPagamento='" + formaPagamento + '\'' +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
