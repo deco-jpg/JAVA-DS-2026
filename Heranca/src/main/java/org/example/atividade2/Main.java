e1c39fc90f1c26da621c5fdd6223315889fec16b
Danilo Freitas <srdaniloh@gmail.com>
2026-03-11 11:57:08 -0300

    heranca update

--- Heranca/src/main/java/org/example/atividade2/Main.java
+++ Heranca/src/main/java/org/example/atividade2/Main.java
@@ -3,7 +3,7 @@ package org.example.atividade2;
 public class Main {
     public static void main(String[] args) {
 
-        PF pf = new PF("Alvaro", "71987331646", "97245455635", "97245455635", "23/02/2000");
+        PF pf = new PF("Danilo", "71987331646", "97245455635", "97245455635", "23/02/2000");
         PJ pj = new PJ("SENAI", "71934455645", "15151515141554", "12");
 
         System.out.println(pf.toString());
